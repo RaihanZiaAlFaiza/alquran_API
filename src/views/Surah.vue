19cc918ca982b9297ca63bbde8135179eeeb0387
Raihanzia <raihanziaalfaiza@gmail.com>
2023-05-17 00:29:59 +0700

    first commit

---
+++ src/views/Surah.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+import {ref} from "vue";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      daftarsurah: ref([])
+    }
+  },
+  mounted() {
+    this.getDaftarSurah()
+  },
+  methods: {
+    getDaftarSurah() {
+      axios.get("https://api.quran.com/api/v4/chapters?language=id")
+          .then((respons) =>
+          {
+            console.timeLog(respons)
+            this.daftarsurah = respons.data.chapters
+          })
+          .catch((err) =>
+          {
+            console.log('error' + err)
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  text-align: left;
+}
+
+</style>
